52c58b66708db571be903df41b91576aec9c0c08
wali <karasadwali@gmail.com>
2019-05-06 16:03:42 +0200

    bonjour boris

--- message.js
+++ message.js
@@ -1,19 +1,55 @@
 
 
 
-function textingMessage(){
+function textingMessage(e){
+	e.preventDefault();
 	var message = $('#usermsg').val();
 
-	$('#chatbox').append('<p class="recu">'+ message +'</p>'+ '<div class="clear"></div>');
+	
+
+	$ .ajax ({
+		url: "message.php",
+		data: {message:message, idUser:1, idDiscussion: 1},
+		method: 'POST',
+		success: function(){
+			console.log('ok');
+		}
+	});	
 
 
 }
 
+function getAllMessages(){
+	
+
+	let userId = 1;
+	$.ajax({
+		url: "message.php",
+		data: {idDiscussion:1},
+		method :'POST',
+		dataType: 'json',
+		success: function(data){
+			console.log(data);
+			$('#chatbox').html('');
+			for (var i = 0; i < data.length; i++) {
+				console.log(data.length);
+
+				if(userId== data[i].AuthorId ){
+					$('#chatbox').append('<p class="recu">'+ data[i].Content + '<br><span class="date">'+data[i].Date+'</span> </p> <div class="clear"></div>');	
+
+				}else{
+					$('#chatbox').append('<p class="envoi">'+ data[i].Content + '<br><span class="date">'+data[i].Date+'</span> </p> <div class="clear"></div>');
+				}
+			}
+		}
+	});
+
+}
 
 
 $(document).ready(function()
 {
-    
-    $("#message").on("submit",textingMessage);
-    
+	getAllMessages();
+	$("#message").on("submit",textingMessage);
+    //setInterval(getAllMessages,10000);
 });
--- message.php
+++ message.php
@@ -1,10 +1,41 @@
 <?php
 
-var_dump($_GET);
+include "bdd_connection.php";
 
-var_dump($_SESSION);
 
-$_SESSION['idUser'] = stripslashes(htmlspecialchars($_GET['idUser']));
+if(array_key_exists("idUser", $_POST) 
+    && array_key_exists("message", $_POST) 
+    && array_key_exists("idDiscussion", $_POST)){
 
-session_start();
-$_SESSION['idDiscussion'] = stripslashes(htmlspecialchars($_GET['idDiscussion']));
+		// //insert message
+		 $message = $_POST['message'];
+		 $idUser = $_POST['idUser'];
+		 $idDiscussion = $_POST['idDiscussion'];
+
+ $requete = $pdo->prepare("
+ INSERT INTO `Message`(`Content`, `AuthorId`, `DiscussionId`) VALUES (?,?,?)
+ ");
+ $requete->execute([$message,$idUser,$idDiscussion]);
+    }
+
+
+if(array_key_exists("idDiscussion", $_POST)){ 
+    
+
+$idDiscussion = $_POST['idDiscussion'];
+ $requete = $pdo->prepare("
+ SELECT `Content`, `Date`, `AuthorId` FROM `Message` WHERE DiscussionId = ?
+ ");
+ $requete->execute([$idDiscussion]);
+ $messages = $requete->fetchAll();
+ echo json_encode($messages);
+
+    
+
+};
+
+?>
+
+
+
+ 
